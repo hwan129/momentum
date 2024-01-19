6da3d4dcd0c9bba10a7039a35b51e710eccb88ad
hwan129 <hoanshim01@handong.ac.kr>
2024-01-19 13:02:37 +0900

    js chapter3

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-// ------------------------------------------------------------------------------
-const age = parseInt(prompt("How old are you???")); // parseInt는 숫자만 처리가능
+const hellos = document.getElementsByClassName("hi");
 
-console.log(typeof age, typeof parseInt(age)); // parseInt는 숫자만 처리가능
-console.log(isNaN(age)); // 숫자 아닌게 맞는가
+const title = document.querySelector(".hello h1"); // hello 클래스 안에 h1부르기. 첫 번째 h1
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a positive number!");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  // 1 < age < 2 가 왜 안되는가
-  console.log("You can drink");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise");
-} else if (age === 100) {
-  console.log("Wow, you are wise!");
-} else if (age > 80) {
-  console.log("You can do whatever you want.");
-}
-// ------------------------------------------------------------------------------
+console.log(title);
+
+// // ------------------------------------------------------------------------------
+// document.getElementById("title");
+// console.dir(title);
+// title.innerText = "got you!"; // change text
+// console.log(title.id);
+// console.log(title.className);
+
+// // ------------------------------------------------------------------------------
+// const age = parseInt(prompt("How old are you???")); // parseInt는 숫자만 처리가능
+
+// console.log(typeof age, typeof parseInt(age)); // parseInt는 숫자만 처리가능
+// console.log(isNaN(age)); // 숫자 아닌게 맞는가
+
+// if (isNaN(age) || age < 0) {
+//   console.log("Please write a positive number!");
+// } else if (age < 18) {
+//   console.log("You are too young.");
+// } else if (age >= 18 && age <= 50) {
+//   // 1 < age < 2 가 왜 안되는가
+//   console.log("You can drink");
+// } else if (age > 50 && age <= 80) {
+//   console.log("You should exercise");
+// } else if (age === 100) {
+//   console.log("Wow, you are wise!");
+// } else if (age > 80) {
+//   console.log("You can do whatever you want.");
+// }
+// //------------------------------------------------------------------------------
 // const age = 96;
 // function calculateKrAge(ageOfForeigner) {
 //   return ageOfForeigner + 2;
@@ -26,7 +39,7 @@ if (isNaN(age) || age < 0) {
 
 // const krAge = calculateKrAge(age);
 // console.log(krAge);
-// ------------------------------------------------------------------------------
+// //------------------------------------------------------------------------------
 // const player = {
 //   name: "hwan",
 //   sayHello: function (name) {
@@ -38,7 +51,7 @@ if (isNaN(age) || age < 0) {
 // player.sayHello("hwan");
 
 // const week = ["mon", "tue", "wed", "thr", "fri", "sat", "sun"];
-// ------------------------------------------------------------------------------
+// //------------------------------------------------------------------------------
 // const calculator = {
 //   add: function (a, b) {
 //     return a + b;
