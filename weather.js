46f1017bf9be9e934a3be627541115b53f8c99dd
hwan129 <hoanshim01@handong.ac.kr>
2024-01-22 12:12:51 +0900

    weather

---
+++ weather.js
@@ -0,0 +1,23 @@
+const API_KEY = "90c0aebcf365086b171bae817b065dba";
+
+function onGeoOk(position) {
+  // 찾는 것 성공
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}도`;
+    });
+  console.log(url);
+}
+function onGeoError() {
+  // 찾는 것 실패
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
